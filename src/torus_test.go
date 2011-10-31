9564cc622ca6753e52cb5fece1bf0ac302bd403a
Ivan Krasin <imkrasin@gmail.com>
2011-10-31 00:12:47 -0700

    Fix torus_test.go (after introducing type Torus)

--- src/torus_test.go
+++ src/torus_test.go
@@ -5,26 +5,29 @@ import (
 )
 
 type GuessDirTest struct {
+	T                Torus
 	FromRow, FromCol int
 	ToRow, ToCol     int
-	Cols             int
 	Dir              Direction
 }
 
+var t4 = Torus{4, 4}
+var t32 = Torus{32, 32}
+
 var guessDirTests = []GuessDirTest{
-	{1, 1, 1, 2, 4, East},
-	{1, 2, 1, 1, 4, West},
-	{3, 3, 0, 3, 4, South},
-	{0, 0, 3, 0, 4, North},
-	{1, 0, 1, 3, 4, West},
-	{31, 1, 31, 0, 32, West},
+	{t4, 1, 1, 1, 2, East},
+	{t4, 1, 2, 1, 1, West},
+	{t4, 3, 3, 0, 3, South},
+	{t4, 0, 0, 3, 0, North},
+	{t4, 1, 0, 1, 3, West},
+	{t32, 31, 1, 31, 0, West},
 }
 
 func TestGuessDir(t *testing.T) {
 	for _, test := range guessDirTests {
-		from := Loc(test.FromRow, test.FromCol, test.Cols)
-		to := Loc(test.ToRow, test.ToCol, test.Cols)
-		if d := GuessDir(from, to, test.Cols); d != test.Dir {
+		from := test.T.Loc(test.FromRow, test.FromCol)
+		to := test.T.Loc(test.ToRow, test.ToCol)
+		if d := test.T.GuessDir(from, to); d != test.Dir {
 			t.Errorf("from: %v, to: %v, want: %c, got: %c", from, to, test.Dir, d)
 		}
 	}
