3c5a428430e3e7a0105d8283c499f2620c459e0e
Ivan Krasin <imkrasin@gmail.com>
2011-10-30 23:14:28 -0700

    Remove richman stuff

--- src/MyBot.go
+++ src/MyBot.go
@@ -1,20 +1,10 @@
 package main
 
 import (
-	"fmt"
+	//"fmt"
 	//	"io/ioutil"
 	"os"
 	"rand"
-)
-
-const (
-	RichmanUnknown   = 10000
-	RichmanEnemy     = 1000000
-	RichmanFood      = 2000000
-	RichmanMyHill    = 2
-	RichmanEnemyHill = 3000000
-
-	RichmanCount = 100
 )
 
 type MyBot struct {
@@ -29,39 +19,6 @@ func (b *MyBot) Init(p Params) (err os.Error) {
 	b.p = p
 	b.m = NewMap(p.Rows, p.Cols, p.ViewRadius2)
 	return nil
-}
-
-func (b *MyBot) GenerateRichman() (r *Richman) {
-	r = NewRichman(b.m.Rows, b.m.Cols)
-	for loc, terrain := range b.m.Terrain {
-		switch terrain {
-		case Water:
-			r.Remove(Location(loc))
-		case Unknown:
-			r.PinVal(Location(loc), RichmanUnknown)
-		}
-	}
-	for _, item := range b.m.Items[b.m.Turn()].All {
-		switch item.What {
-		case Ant:
-			if item.Owner != Me {
-				r.PinVal(item.Loc, RichmanEnemy)
-			}
-		case Food:
-			r.PinVal(item.Loc, RichmanFood)
-		case Hill:
-			if item.Owner == Me {
-				r.PinVal(item.Loc, RichmanMyHill)
-			} else {
-				r.PinVal(item.Loc, RichmanEnemyHill)
-			}
-		case DeadAnt:
-		default:
-			panic(fmt.Sprintf("Unknown item: %c", item.What))
-		}
-	}
-	r.Iterate(RichmanCount)
-	return r
 }
 
 func (b *MyBot) DoTurn(input []Input) (orders []Order, err os.Error) {
@@ -141,25 +98,6 @@ func (b *MyBot) DoTurn(input []Input) (orders []Order, err os.Error) {
 
 	b.cn.Dump("/tmp/country.txt")
 
-	//	r := b.GenerateRichman()
-
-	/*	for _, ant := range b.m.MyLiveAnts {
-		bestD := Direction(North)
-		bestR := r.Val(ant.Loc(turn))
-		for _, d := range dirs {
-			if b.m.CanMove(ant.Loc(turn), d) {
-				newR := r.Val(b.m.NewLoc(ant.Loc(turn), d))
-				if newR > bestR {
-					bestD = d
-					bestR = newR
-				}
-			}
-		}
-
-		if b.m.CanMove(ant.Loc(turn), bestD) {
-			b.m.Move(ant, bestD)
-		}
-	}*/
 	for _, ant := range b.m.MyLiveAnts {
 		if ant.HasLoc(turn + 1) {
 			// This ant has been moved
