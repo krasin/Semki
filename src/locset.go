3054895a96eb63d5a368798472f34cd6202931c1
Ivan Krasin <imkrasin@gmail.com>
2011-11-01 23:19:55 +0000

    gofmt

--- src/locset.go
+++ src/locset.go
@@ -1,28 +1,28 @@
 package main
 
 type LocSet interface {
-     Add(loc Location)
-     Has(loc Location) bool
-     Clear()
+	Add(loc Location)
+	Has(loc Location) bool
+	Clear()
 }
 
 type locSet struct {
-     a []int
-     b int
+	a []int
+	b int
 }
 
 func NewLocSet(size int) LocSet {
-     return &locSet{ a: make([]int, size) }
+	return &locSet{a: make([]int, size)}
 }
 
 func (s *locSet) Clear() {
-     s.b++
+	s.b++
 }
 
 func (s *locSet) Add(loc Location) {
-     s.a[loc] = s.b
+	s.a[loc] = s.b
 }
 
 func (s *locSet) Has(loc Location) bool {
-     return s.a[loc] == s.b
+	return s.a[loc] == s.b
 }
