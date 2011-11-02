2df2a5f3f1f0eca8f09b61bb156e51d8670ed13c
Ivan Krasin <imkrasin@gmail.com>
2011-11-02 10:04:34 -0700

    Don't include _test.go files into the bot. Server will fail to compile such zip

--- pack.sh
+++ pack.sh
@@ -3,7 +3,12 @@
 set -e
 
 rm -rf SemkiBot.zip
-cd src
-zip -r ../SemkiBot.zip *
+cur=`pwd`
+tmp=`tempfile`
+rm $tmp
+cp -rf src $tmp
+cd $tmp
+rm *_test.go
+zip -r $cur/SemkiBot.zip *
 
 echo "Done. SemkiBot.zip has been created."
